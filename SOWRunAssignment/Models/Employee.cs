f5319edf880a5b920a3351b9c5c275cf3bdebe0d
Nirbhay Singh <kumarnirbhay1986@gmail.com>
2024-01-03 01:56:03 -0600

    Updated

--- SOWRunAssignment/Models/Employee.cs
+++ SOWRunAssignment/Models/Employee.cs
@@ -9,6 +9,7 @@ namespace SOWRunAssignment.Models
     {
         [Required(ErrorMessage = "Employee Number is required")]
         [StringLength(15,ErrorMessage = "Employee Number not more than 15 digit")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Employee Number must be numeric")]
         [DisplayName("Employee Number")]
         public string Empno { get; set; }
 
@@ -17,8 +18,8 @@ namespace SOWRunAssignment.Models
         public string  Ename { get; set; }
         [Required(ErrorMessage = "Employee Email Id is required")]
         [DisplayName("Employee Email Address")]
-        [DataType(DataType.EmailAddress)]
-        [EmailAddress]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid e-mail adress")]
+         
         public string EmailID { get; set; }
     }
 }
